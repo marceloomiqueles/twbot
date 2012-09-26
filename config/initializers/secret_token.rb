6f4849715a69d39cda920f2715e08ed285fc1cdb
Leonardo <leonardo311@gmail.com>
2012-09-26 13:18:13 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twbot::Application.config.secret_token = '9c4b779f1c50ae46bfdc845220fd565b07dcfaa709fc02a22d7c536da04b6084c657681dacb45d6059bb01abba1ace909472fbfaff5bf9c6ebd00b1f604355ba'
