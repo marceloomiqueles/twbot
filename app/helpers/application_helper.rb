a89cd5aad2b4c522a5de35331512340575a83bd6
Leonardo <leonardo311@gmail.com>
2012-10-02 12:26:38 -0300

    Cambio de nombre

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	# Return the full title on a per-page basis.
 	def full_title(page_title)
-		base_title = "TW Bot"
+		base_title = "Followkeywords"
 		if page_title.empty?
 			base_title
 		else
